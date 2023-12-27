7102cbd6ab11d650549413c6ed6b0d1f333c87fc
Arthur G. <arturgamidov2705@gmail.com>
2023-12-27 18:58:58 +0200

    Added Food Prefab and Food Prefab Spawner.

--- Assets/Scripts/Food/Food.cs
+++ Assets/Scripts/Food/Food.cs
@@ -1,19 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
+using System;
 
-public class Food : MonoBehaviour
+public class Food : NetworkBehaviour
 {
     [SerializeField] GameObject particlePrefab;
 
+    public static event Action ServerOnFoodEaten;
+
     void OnTriggerEnter(Collider other)
     {
-        if (!other.CompareTag("Player")) return;
+        if (!other.CompareTag("Player"))
+        {
+            return; 
+        } 
+        
         FindObjectOfType<Snake>().AddTail();
         GameObject boom = Instantiate
             (particlePrefab, transform.position, particlePrefab.transform.rotation);
         Destroy(boom, 3f);
-        Destroy(gameObject);
-        FindObjectOfType<FoodSpawner>().SpawnFood();
+        NetworkServer.Destroy(gameObject);
+        ServerOnFoodEaten?.Invoke();
     }
 }
--- Assets/Scripts/SnakeNetworkManager.cs
+++ Assets/Scripts/SnakeNetworkManager.cs
@@ -5,5 +5,19 @@ using UnityEngine;
 
 public class SnakeNetworkManager : NetworkManager
 {
-    
+    [SerializeField] GameObject FoodSpawnerPrefab;
+
+    public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        if (numPlayers != 2)
+        {
+            return;
+        }
+
+        var FoodSpawnerInstance = Instantiate(FoodSpawnerPrefab);
+        NetworkServer.Spawn(FoodSpawnerInstance);
+    }
+
 }
